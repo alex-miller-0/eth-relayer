43cd08d25c0925b117d5699b450233fe81da3435
Alex Miller <asmiller1989@gmail.com>
2017-12-21 14:00:23 -0800

    Adds auto-signature to relay messages. This will be used to execute relays by users

--- daemon/lib/signing.js
+++ daemon/lib/signing.js
@@ -23,9 +23,33 @@ function sign(msg, index=0) {
     r: `0x${leftPad(sigTmp.r.toString('hex'), 64, '0')}`,
     s: `0x${leftPad(sigTmp.s.toString('hex'), 64, '0')}`,
     v: sigTmp.v,
+    hash: msg,
   };
   return newSig;
 }
 
+// Hash a set of data and return the signature
+function hashAndSign(data) {
+  const hash = _hashData(data);
+  const sig = sign(hash);
+  return sig;
+}
+
+function _hashData(data) {
+  // NOTE: Solidity tightly packs addresses as 20-byte strings. Everything else
+  // is packed as a 32 byte string. This is a weird idiosyncracy.
+  const a = data.fromChain.toLowerCase().slice(2);
+  const b = data.toChain.toLowerCase().slice(2);
+  const c = data.oldToken.toLowerCase().slice(2);
+  const e = leftPad(parseInt(data.amount, 10).toString(16), 64, '0');
+  const f = data.sender.toLowerCase().slice(2);
+  const g = leftPad(parseInt(data.fee, 10).toString(16), 64, '0');
+  const h = leftPad(parseInt(data.timestamp, 10).toString(16), 64, '0');
+  const msg = `${a}${b}${c}${e}${f}${g}${h}`;
+  const personal = ethutil.hashPersonalMessage(Buffer.from(msg, 'hex'));
+  return `0x${personal.toString('hex')}`;
+}
+
+exports.hashAndSign = hashAndSign;
 exports.getAddr = getAddr;
 exports.sign = sign;
